f9f9906fc35ec90575b70c1c0b5a1c86a72ca940
Shaurya Aher <72655598+shauryaaher@users.noreply.github.com>
2021-05-19 12:44:06 +0530

    Use Express Parser

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const port = 5000;
 
-const parser = bodyParser.urlencoded({
+const parser = express.urlencoded({
     extended: true
 });
 
